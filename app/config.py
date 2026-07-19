9f81bd76a82d282b2579d2d9340d33a85ed7b1aa
Xitiz Magar <xitizmgr1234567890@gmail.com>
2026-07-19 21:51:24 +0545

    Initial commit

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config():
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = "app/static/uploads/receipts"
+    MAX_CONTENT_LENGTH = 5 * 1024 * 1024
+    ALLOWED_EXTENSIONS = {
+    "png",
+    "jpg",
+    "jpeg",
+    "pdf"
+}
+    
---
+++ app/models/account.py
@@ -0,0 +1,13 @@
+from app.extensions import db
+class Account(db.Model):
+    __tablename__ = "accounts"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    account_name = db.Column(db.String(100), nullable=False)
+
+    account_type = db.Column(db.String(50), nullable=False)
+
+    balance = db.Column(db.Float, default=0)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
