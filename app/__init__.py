ccdc2ab2eda320388ec64c71ea0caa8f5c38673f
Xitiz Magar <xitizmgr1234567890@gmail.com>
2026-08-24 17:35:42 +0545

    So the crf variable which was creating the token was deleted idk how i think i ctl z there TwT and wrote the routing dash.dashborad instead of dashboard.dashborad i fixed it and change some minor ux problem

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+    csrf.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
     app.register_blueprint(auth_bp)
